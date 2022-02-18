cdff9415e476bd89bc9e357398317fceb996f59a
Khoa9317 <97727616+Khoa9317@users.noreply.github.com>
2022-02-18 14:58:53 +0700

    Add files via upload

--- Hello World/src/vergil/Bt2.java
+++ Hello World/src/vergil/Bt2.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Bt2 {
 	public static void main(String[]args) {
 		Scanner scanner =new Scanner (System.in);
-		int a =scanner.nextByte();
-		int b=scanner.nextByte();
+		int a =scanner.nextInt();
+		int b=scanner.nextInt();
 		System.out.println(a+b);
 		System.out.println(a-b);
 		System.out.println(a*b);
--- Hello World/src/vergil/Bt3.java
+++ Hello World/src/vergil/Bt3.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Bt3 {
 	public static void main(String[]args) {
 		Scanner scanner =new Scanner (System.in);
-		double a =scanner.nextByte();
-		double b=scanner.nextByte();
+		double a =scanner.nextDouble();
+		double b=scanner.nextDouble();
 		System.out.println(a+b);
 		System.out.println(a-b);
 		System.out.println(a*b);
